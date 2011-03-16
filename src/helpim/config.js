3681a59094581eb80e88687c32228ca20cc9d173
Stefan Strigler <stefan@strigler.de>
2011-03-16 18:09:31 +0100

    import all props from config

--- src/helpim/config.js
+++ src/helpim/config.js
@@ -4,8 +4,15 @@ goog.provide('helpim.Config');
  * @constructor
  */
 helpim.Config = function(obj) {
+    for (var i in obj) {
+        if (obj.hasOwnProperty(i)) {
+            this[i] = obj[i];
+        }
+    }
+
     // set some reasonable defaults
     this.httpbase = obj.httpbase || '/http-bind/';
     this.xmppdomain = obj.xmppdomain || 'localhost';
     this.resource = obj.resource || 'helpim';
+
 };
