3dd858c2518dc209c2313ae5ff3061c92960e1b3
Stefan Strigler <stefan@strigler.de>
2011-03-16 18:19:07 +0100

    enable debugging

--- src/helpim/helpim.js
+++ src/helpim/helpim.js
@@ -1,8 +1,12 @@
 goog.provide('helpim.start');
 
 goog.require('helpim.Client');
+goog.require('goog.debug.Console');
 
 helpim.start = function() {
+    if (goog.DEBUG) {
+        new goog.debug.Console().setCapturing(true);
+    }
     new helpim.Client(new helpim.Config(HELPIM_CONFIG));
 };
 
