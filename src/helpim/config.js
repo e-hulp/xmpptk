39e2939caccab307276cacdd89a027dda82cab66
Stefan Strigler <stefan@strigler.de>
2011-03-24 11:58:57 +0100

    jsjac has 'domain' instead of 'xmppdomain'

--- src/helpim/config.js
+++ src/helpim/config.js
@@ -12,7 +12,7 @@ helpim.Config = function(obj) {
 
     // set some reasonable defaults
     this.httpbase = obj.httpbase || '/http-bind/';
-    this.xmppdomain = obj.xmppdomain || 'localhost';
+    this.domain = obj.domain || 'localhost';
     this.resource = obj.resource || 'helpim';
 
 };
