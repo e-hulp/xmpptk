934124f697f97483805f14159a593b888bd32686
Stefan Strigler <stefan@strigler.de>
2011-03-24 20:00:08 +0100

    renamed this._logger fixed creating room fix onunload

--- src/helpim/client.js
+++ src/helpim/client.js
@@ -1,42 +1,41 @@
 goog.provide('helpim.Client');
 
-goog.require('xmpptk.Client');
-
 goog.require('goog.dom');
 goog.require('goog.events');
 goog.require('goog.events.EventType');
 goog.require('goog.debug.Logger');
+
+goog.require('xmpptk.Client');
+goog.require('xmpptk.muc.Room');
 
 /**
  * @constructor
  * @extends {xmpptk.Client}
  */
 helpim.Client = function() {
-    this.logger.info("starting up");
+    this._logger.info("starting up");
     xmpptk.Client.call(this);
 
     this.login(
         function() {
-            this.logger.info("logged in successfully");
-            var room = new xmpptk.muc.Room({room:    cfg.muc_room,
-                                            service: cfg.muc_service,
-                                            nick:    cfg.muc_nick});
+            this._logger.info("logged in successfully");
+            this.room = new xmpptk.muc.Room({room:    xmpptk.Config.muc_room,
+                                            service: xmpptk.Config.muc_service,
+                                            nick:    xmpptk.Config.muc_nick},
+                                           this);
 
-            room.join();
+            this.room.join();
         },
         this
     );
 
     goog.events.listen(
-        goog.dom.getElementsByTagNameAndClass('body')[0],
+        window,
         goog.events.EventType.UNLOAD,
-        goog.bind(
-            function() {
-                this.logger.info("logging out");
-                this.logout();
-            },
-            this
-        )
+        goog.bind(function() {
+            this.room.part();
+            this.logout();
+        }, this)
     );
 };
 goog.inherits(helpim.Client, xmpptk.Client);
@@ -46,4 +45,4 @@ goog.addSingletonGetter(helpim.Client);
  * @protected
  * @type {goog.debug.Logger}
  */
-helpim.Client.prototype.logger = goog.debug.Logger.getLogger('helpim.Client');
+helpim.Client.prototype._logger = goog.debug.Logger.getLogger('helpim.Client');
