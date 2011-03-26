5d7d9e70c6c2db3d7101469319fcbd229ac111f2
Stefan Strigler <stefan@strigler.de>
2011-03-26 13:40:37 +0100

    some initial ui elements (login/out button)

--- src/helpim/client.js
+++ src/helpim/client.js
@@ -8,6 +8,8 @@ goog.require('goog.debug.Logger');
 goog.require('xmpptk.Client');
 goog.require('xmpptk.muc.Room');
 
+goog.require('helpim.ui.Client');
+
 /**
  * @constructor
  * @extends {xmpptk.Client}
@@ -16,26 +18,14 @@ helpim.Client = function() {
     this._logger.info("starting up");
     xmpptk.Client.call(this);
 
-    this.login(
-        function() {
-            this._logger.info("logged in successfully");
-            this.room = new xmpptk.muc.Room({room:    xmpptk.Config.muc_room,
-                                             service: xmpptk.Config.muc_service,
-                                             nick:    xmpptk.Config.muc_nick},
-                                            this);
-
-            this.room.join();
-        },
-        this
-    );
+    new helpim.ui.Client(this);
 
     goog.events.listen(
         window,
         goog.events.EventType.UNLOAD,
-        goog.bind(function() {
-            this.room.part();
-            this.logout();
-        }, this)
+        this.logout,
+        false, 
+        this
     );
 };
 goog.inherits(helpim.Client, xmpptk.Client);
@@ -46,3 +36,27 @@ goog.addSingletonGetter(helpim.Client);
  * @type {goog.debug.Logger}
  */
 helpim.Client.prototype._logger = goog.debug.Logger.getLogger('helpim.Client');
+
+helpim.Client.prototype.login = function() {
+    goog.base(
+        this,
+        'login', 
+        function() {
+            this._logger.info("logged in successfully");
+            this.room = new xmpptk.muc.Room({room:    xmpptk.Config.muc_room,
+                                             service: xmpptk.Config.muc_service,
+                                             nick:    xmpptk.Config.muc_nick},
+                                            this);
+            this.room.join();
+        },
+        this
+    );
+};
+
+helpim.Client.prototype.logout = function() {
+    if (this.room) {
+        this.room.part();
+        delete this.room;
+    }
+    goog.base(this, 'logout');
+};
--- src/helpim/helpim.js
+++ src/helpim/helpim.js
@@ -1,6 +1,6 @@
 goog.provide('helpim.start');
 
-goog.require('goog.debug.Console');
+goog.require('goog.debug.DivConsole');
 goog.require('goog.object');
 
 goog.require('xmpptk.Config');
@@ -9,7 +9,10 @@ goog.require('helpim.Client');
 
 helpim.start = function(cfg) {
     if (goog.DEBUG) {
-        new goog.debug.Console().setCapturing(true);
+        goog.debug.LogManager.getRoot().setLevel(goog.debug.Logger.Level.ALL);
+        var logger = goog.debug.Logger.getLogger('helpim');
+        var logconsole = new goog.debug.DivConsole(goog.dom.getElement('log'));
+        logconsole.setCapturing(true);
     }
     goog.object.extend(xmpptk.Config, cfg);
     helpim.Client.getInstance();
