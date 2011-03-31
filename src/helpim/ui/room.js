7b781eb55119b39ac282e36e842523e1e1749d45
Stefan Strigler <stefan@strigler.de>
2011-03-31 11:00:27 +0200

    allow sending messages to room

--- src/helpim/ui/room.js
+++ src/helpim/ui/room.js
@@ -1,6 +1,10 @@
 goog.provide('helpim.ui.Room');
 
 goog.require('goog.debug.Logger');
+goog.require('goog.events');
+goog.require('goog.events.EventType');
+goog.require('goog.ui.Textarea');
+goog.require('goog.ui.TextareaRenderer');
 
 goog.require('xmpptk.ui');
 goog.require('xmpptk.ui.View');
@@ -18,7 +22,34 @@ helpim.ui.Room = function(room) {
 
     this._subjectPanel  = goog.dom.getElementByClass('subjectPanel', this._panel);
     this._messagesPanel = goog.dom.getElementByClass('messagesPanel', this._panel);
-
+    this._sendTextarea = new goog.ui.Textarea("Type here to send a message");
+    this._sendTextarea.render(goog.dom.getElementByClass('sendTextarea', this._panel));
+    this._sendTextarea._firstClick = true;    
+    goog.events.listen(
+        this._sendTextarea.getContentElement(),
+        goog.events.EventType.CLICK,
+        goog.bind(function(e) {
+            if (this._sendTextarea._firstClick) {
+                this._sendTextarea.setValue('');
+                this._sendTextarea._firstClick = false;
+            }
+        }, this)
+    );
+    goog.events.listen(
+        this._sendTextarea.getContentElement(),
+        goog.events.EventType.KEYPRESS,
+        goog.bind(function(e) {
+            this._logger.info(e.charCode);
+            this._logger.info(this._sendTextarea.getValue());
+            if (e.charCode == 13) { // return key
+                try {
+                    room.sendMessage(this._sendTextarea.getValue());
+                    this._sendTextarea.setValue('');
+                    e.preventDefault();
+                } catch(e) { this._logger.severe("failed sending message", e); }
+            }
+        }, this)
+    );
     this._messagesAt = 0;
 };
 goog.inherits(helpim.ui.Room, xmpptk.ui.View);
