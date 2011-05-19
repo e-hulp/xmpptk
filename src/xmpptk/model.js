f5c35ea3ffd7dde62d0e6da6083024b32c7de28a
Stefan Strigler <stefan@strigler.de>
2011-05-19 16:47:56 +0200

    new method 'attachPropertyhandler' to listen to specific properties to change
    added some more comments

--- src/xmpptk/model.js
+++ src/xmpptk/model.js
@@ -2,6 +2,7 @@ goog.provide('xmpptk.Model');
 
 goog.require('xmpptk.Subject');
 goog.require('goog.object');
+goog.require('goog.array');
 
 /**
  * @constructor
@@ -9,9 +10,32 @@ goog.require('goog.object');
  */
 xmpptk.Model = function() {
     xmpptk.Subject.call(this);
+    /** @private */
+    this._propertyHandlers = {};
 };
 goog.inherits(xmpptk.Model, xmpptk.Subject);
 
+/**
+ * Attach a handler to a property.
+ * @param {string} property the property to attach to
+ * @param {function(string, object)} handler the handler to be called when property is being updated
+ * @param {?object} context context to bind handler to (the this reference within the handler)
+ */
+xmpptk.Model.prototype.attachPropertyhandler = function(property, handler, context) {
+    if (!this._propertyHandlers[property]) {
+        this._propertyHandlers[property] = [];
+    }
+    if (context) {
+        handler = goog.bind(handler, context);
+    }
+    this._propertyHandlers[property].push(handler);
+};
+
+/**
+ * retrieve the value of a property
+ * @param {string} prop the property to retrieve the value for
+ * @return {object} whatever has been assigned to the property
+ */
 xmpptk.Model.prototype.get = function(prop) {
     if (typeof prop == 'undefined') {
         // return object with all of our properties
@@ -33,6 +57,12 @@ xmpptk.Model.prototype.get = function(prop) {
     throw "property not found: "+prop;
 };
 
+/**
+ * Updates the value of a property
+ * @param {string} prop the property to be updated
+ * @param {object} value the value to assign to the property
+ * @param {?boolean} skip_notify whether to not notify observers about this update
+ */
 xmpptk.Model.prototype.set = function(prop, value, skip_notify) {
     if (!prop) {
         // I don't see a reason how prop could be null here but
@@ -54,8 +84,21 @@ xmpptk.Model.prototype.set = function(prop, value, skip_notify) {
     } else if (prop.indexOf('_') != 0) { // skip hidden props
         this[prop] = value;
     }
+
     if (!skip_notify) {
-        this.notify();
+        // notify observers registered by observer pattern
+        this.notify(prop);
+
+        // notify propery handlers
+        if (goog.isArray(this._propertyHandlers[prop])) {
+            goog.array.forEach(
+                this._propertyHandlers[prop],
+                goog.bind(function(handler) {
+                    handler(prop, this.get(prop));
+                }, this)
+            );
+        }
     }
+
     return this;
 };
