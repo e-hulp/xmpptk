2b1bbf28308514555c46a450b99fc06b95fa4427
Stefan Strigler <stefan@strigler.de>
2011-06-23 15:00:35 +0200

    use console logger

--- src/helpim/helpim.js
+++ src/helpim/helpim.js
@@ -1,9 +1,9 @@
 goog.provide('helpim');
 
-goog.require('goog.debug.DivConsole');
 goog.require('goog.debug.Logger');
 goog.require('goog.debug.Logger.Level');
 goog.require('goog.debug.LogManager');
+goog.require('goog.debug.Console');
 
 goog.require('xmpptk.Config');
 
@@ -13,10 +13,14 @@ goog.require('helpim.Client');
  * @param {xmpptk.Config} cfg a configuration 
  */
 helpim.start = function(cfg) {
-    if (goog.DEBUG) {
-        goog.debug.LogManager.getRoot().setLevel(goog.debug.Logger.Level.ALL);
+    if (cfg.debug) {
+        var log_level = goog.debug.Logger.Level.ALL;
+        if (goog.isString(cfg.log_level) && goog.debug.Logger.Level[cfg.log_level.toUpperCase()]) {
+            log_level = goog.debug.Logger.Level[cfg.log_level.toUpperCase()];
+        }
+        goog.debug.LogManager.getRoot().setLevel(log_level);
         var logger = goog.debug.Logger.getLogger('helpim');
-        var logconsole = new goog.debug.DivConsole(goog.dom.getElement('log'));
+        var logconsole = new goog.debug.Console();
         logconsole.setCapturing(true);
     }
     goog.object.extend(xmpptk.Config, cfg);
