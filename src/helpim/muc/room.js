bd90022b5d0f93d8569266ab35e1f6ea9d5e2b36
Stefan Strigler <stefan@strigler.de>
2011-08-31 18:29:57 +0200

    moved blockParticipant from client to room

--- src/helpim/muc/room.js
+++ src/helpim/muc/room.js
@@ -14,6 +14,29 @@ goog.inherits(helpim.muc.Room, xmpptk.muc.Room);
 
 helpim.muc.Room.prototype._logger = goog.debug.Logger.getLogger('helpim.muc.Room');
 
+/**
+ * send iq to bot to advise blocking of participant related to given jid.
+ * must be staff to do so.
+ * @param {string} bot the jid of the bot to talk to
+ * @param {string} participant the jid of the participant to block
+ */
+helpim.Client.prototype.blockParticipant = function(bot, participant) {
+    if (!xmpptk.Config['is_staff']) {
+        // no need to try cause bot would cancel the request anyway
+        return;
+    }
+    var iq = new JSJaCIQ();
+    iq.setTo(bot);
+    iq.setType('set');
+    iq.appendNode('block', {xmlns: helpim.Client.NS.HELPIM_ROOMS}, [
+        iq.buildNode('participant', {xmlns: helpim.Client.NS.HELPIM_ROOMS}, participant)
+    ]);
+    this._con.sendIQ(iq, {
+        'result_handler': function() {},
+        'error_handler': function() {}
+    });
+}
+
 helpim.muc.Room.prototype.part = function(cb) {
     // unregister handlers
     this._client.unregisterRoom(this);
