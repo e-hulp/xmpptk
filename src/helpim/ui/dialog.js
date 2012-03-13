8da6cfbbaa72f3f428d493e2c063e5c3f1fcf8ed
Stefan Strigler <stefan@strigler.de>
2012-03-13 12:40:57 +0100

    fix: badly implemented Array.prototype.splice at IE needs second argument
    see also http://www.coshima.com/2011/02/02/array-splice-in-internet-explorer/

--- src/helpim/ui/dialog.js
+++ src/helpim/ui/dialog.js
@@ -13,13 +13,19 @@ goog.require('goog.ui.Dialog');
 helpim.ui.Dialog = function(opt_class, opt_useIframeMask, opt_domHelper) {
     opt_class = opt_class || "modal-dialog";
     opt_class = opt_class.split(" ");
-    this.classes_ = opt_class.splice(1);
+    // fucking IE needs a second argument to splice - see also
+    // http://www.coshima.com/2011/02/02/array-splice-in-internet-explorer/
+    this._classes = opt_class.splice(1, opt_class.length-1);
+    this._logger.info(this._classes);
+
     goog.ui.Dialog.call(this, opt_class, opt_useIframeMask, opt_domHelper);
 };
 goog.inherits(helpim.ui.Dialog, goog.ui.Dialog);
 
+helpim.ui.Dialog.prototype._logger = goog.debug.Logger.getLogger('helpim.ui.Dialog');
+
 /** @inheritDoc */
 helpim.ui.Dialog.prototype.createDom = function() {
     goog.base(this, 'createDom');
-    goog.dom.classes.add(this.getElement(), this.classes_);
+    goog.dom.classes.add(this.getElement(), this._classes);
 };
